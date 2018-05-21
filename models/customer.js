1c7e36b5392455177e008954f7663e08339deb67
axayhegde <akshayheg@gmail.com>
2018-05-21 12:01:06 +0530

    added validations for all fields

--- models/customer.js
+++ models/customer.js
@@ -2,15 +2,44 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var customerSchema = new Schema({
-    customerId: { type: String, required: true, index: { unique: true } },
-    customerEmail : { type: String, require: true},
-    firstName: { type: String, required: true},
-    lastName: { type: String, required: true },
-    phoneNumber: { type: String, required: true },
-    password: { type: String, required: true },
-    confirmPassword: { type: String, required: true },
-    address: { type: String, required: true },
-
+    customerId: {
+        type: String,
+        required: true,
+        index: { unique: true }
+        },
+    customerEmail : {
+        type: String,
+        require: 'Email Address is required',
+        match :/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    },
+    firstName: {
+        type: String,
+        required: true,
+        match : /^[a-z ,.'-]{1,25}$/i
+    },
+    lastName: {
+        type: String,
+        required: true,
+        match : /^[a-z ,.'-]{1,25}$/i
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+        match : /^[0-9]{10}$/
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    confirmPassword: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true,
+        match : /^[a-z 0-9,.'-]{20,300}$/i
+    }
 });
 
 module.exports = mongoose.model('Customer',customerSchema);
--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -7,14 +7,35 @@ var Restaurant = require('../models/restaurant');
 router.post('/register',function(req,res){
     var customer = new Customer();
 
+    let nameRegex = /^[a-z ,.'-]{1,25}$/i;
+    let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let phoneNumberRegex = /^[0-9]{10}$/;
+    let addressRegex = /^[a-z 0-9,.'-]{20,300}$/i;
+
     customer.customerId = req.body.customerId;
+
     customer.firstName = req.body.firstName;
+    if(!customer.firstName.match(nameRegex)){
+        if(customer.firstName.length > 25) throw 'first name can\'t be more than 25 characters';
+        else throw 'First name invalid, no special characters allowed';
+    }
+
     customer.lastName = req.body.lastName;
+    if(!customer.lastName.match(nameRegex)){
+        if(customer.lastName.length > 25) throw 'first name can\'t be more than 25 characters';
+        else throw 'Last name invalid, no special characters allowed';
+    }
+
     customer.email = req.body.email;
+    if(!customer.email.match(emailRegex)) throw 'Invalid email';
+
     customer.phoneNumber = req.body.phoneNumber;
+    if(!customer.phoneNumber.match(phoneNumberRegex)) throw 'Invalid phone number, enter 10 digits, no spaces';
+
     customer.password = req.body.password;
     customer.confirmPassword = req.body.confirmPassword;
     customer.address = req.body.address;
+    if(!customer.address.match(addressRegex)) throw 'Invalid Address';
 
     customer.save(function(err){
         if(err) throw err;
@@ -31,6 +52,20 @@ router.get('/home', function(req, res) {
         } else {
             console.log(restaurants);
             res.json(restaurants);
+            res.end();
+        }
+    })
+});
+
+//View Customer for viewing all restaurants
+
+router.get('/viewCustomer', function(req, res) {
+    Customer.find({}, function (err, customers) {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(customers);
+            res.json(customers);
             res.end();
         }
     })
