e2d6cca11294f14e0529822dd0b318c1461447f8
Rohan Paul <rohanpaul2@gmail.com>
2018-05-23 11:39:51 +0530

    rohan-multer-file-upload

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var db = require('./dbconnect');
 var Order = require('./models/order');
 var order = require('./routes/order');
 var router = express.Router();
+var path = require('path');
 
 var app = express();
 
@@ -13,48 +14,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.text());
 app.use(bodyParser.urlencoded({extended: true}));
 
-/* app.put('/:id', function (req, res) {
-    Order.findById(req.params.id, function(err, user) {
-        if (err) throw err;
+// app.use(express.static(path.join(__dirname, 'public')));
 
-        order.customerId = req.body.customerId;
-        order.driverId = req.body.driverId;
-        order.orderItems = req.body.orderItems;
-        order.selectedRestaurant = req.body.selectedRestaurant;
-        order.createdAt = req.body.createdAt;
-        order.updatedAt = req.body.updatedAt;
-        order.deletedAt = req.body.deletedAt;
-        order.completed = req.body.completed;
-        order.specialInstructions = req.body.specialInstructions;
-
-            order.save(function(err) {
-                if(err) throw err;
-                res.json(user);
-            })
-
-    })
-}) */
-
-// In the below, in Order.remove() I have to select one of the schema property to delete an entire order
-app.delete('/:id', function(req, res) {
-    Order.remove({createdAt: req.params.id}, function(err) {
-        if(err) {
-            throw err;
-        }
-        res.json({"Status" : "Successfully Deleted"});
-    });
-});
+app.use('/', express.static(__dirname + '/public'))
 
 // Use Routes
 app.use('/', order);
-
-/* app.get('/', function(req, res) {
-    Order.find({}, function(err, orders) {
-        if(err) throw err;
-        res.json(orders);
-    });
-});
- */
 
 app.listen(3232, function(err){
     if(err) throw err;
