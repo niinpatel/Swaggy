8086d26ab374a9c230bc0a1d308fc5fb36e7336c
axayhegde <akshayheg@gmail.com>
2018-06-06 10:26:35 +0530

    removed some required validation

--- models/restaurant.js
+++ models/restaurant.js
@@ -8,31 +8,31 @@ var restaurantSchema = new Schema({
         restaurantName :{ type: String, required: true},
         restaurantPhoneNumber: { type: String, required: true },
         restaurantEmail: { type: String, required: true},
-        restaurantGeoLocation: { type: String, required: true },
+        restaurantGeoLocation: { type: String},
         restaurantAddress : { type: String, required: true},
         AverageRating : { type: String},
         MinimumOrderPrice : { type: String},
-        DeliveryFee : { type: String, required: true},
+        DeliveryFee : { type: String},
         AverageDeliveryTime : { type: String},
-        OpenStatus : { type: Boolean, required: true},
+        OpenStatus : { type: Boolean},
         reviewEnabled : { type: Boolean,default: false},
 
         deliverable_area :  [{
-            id: {type: String, required : true},
-            AreaName : {type: String, required : true}
+            id: {type: String},
+            AreaName : {type: String}
         }],
         ratings : [{type : Number}],
         restaurantTimings : [{
-            day : {type : String, required: true},
-            timings: {type : String, required: true}
+            day : {type : String},
+            timings: {type : String}
         }],
 
         MenuCategories : [{
           categoryId : {type : String, required : true},
-          categoryName : {type : String},
+          categoryName : {type : String, required : true},
           menuItems : [{
               menuItemId : {type: String, require : true},
-              menuItemName : {type : String},
+              menuItemName : {type : String, required: true},
               menuPrice : {type : String},
               menuItemDesc : {type : String},
               menuItemImages : [{type : String}],
